02265eefdc52ebe86c581c0b1c71cde3b9d58e10
Eyal Shahar <eyal@shahar.co.il>
2025-07-09 10:41:38 -0700

    fix duplicate cannonical

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import '../src/styles/globals.css';
+import { AppProps } from "next/app";
+import Head from "next/head";
+import "../src/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -17,7 +17,6 @@ export default function App({ Component, pageProps }: AppProps) {
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap"
           rel="stylesheet"
         />
-        <link rel="canonical" href="https://overtink.com" />
         <meta name="robots" content="index, follow" />
         <link rel="author" href="https://ey.al" />
         <meta name="author" content="Eyal Shahar" />
